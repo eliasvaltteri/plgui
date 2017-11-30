3c438a673ccc8bfc8b5be2123ef8ce59d9e00092
eliasvaltteri <elvawirp@student.jyu.fi>
2017-12-01 00:02:44 +0100

    somewhat working version

--- renderer.js
+++ renderer.js
@@ -1,3 +1,21 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/softarc";
+var obj;
+
+function searchFunc() {
+
+	var dep = document.getElementById("dept").value;
+	var arr = document.getElementById("dest").value;;
+	var query = { "name": {$regex : dep+" => "+arr} };
+
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		db.collection("routes").find(query).toArray(function(err, result) {
+			if (err) throw err;
+			document.getElementById("route").innerHTML = result[0].line+"";
+			obj = result[0].line;
+			console.log(obj);
+			db.close()
+		});
+	});
+}
